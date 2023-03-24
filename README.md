38fd05f288e570c899410f1d7d22ffd4b00563b2
Patrick Basquel <pbdevpros@gmail.com>
2023-03-23 23:58:34 -0700

    + Experiment with tf probability
    ! Update README

--- README.md
+++ README.md
@@ -1,20 +1,8 @@
 # Exhibition of Mixed Density Networks
 
-# 1. Note on Model Training
+Nueral Nets can be described as function approximators. There are some basic functions however which such a basic NN may fail to correctly approximate, despite experimentation with hyperparameters: where the outputs may be a distribution (i.e. a range of possible outputs).
 
-In short, a neural network (NN) can serve as function approximator. Here is a plot showing a basic NN approximating `sin(x)`:
-
-![Approximating `sin(x)`](/images/01_2x64dense_softmax_100thou_ranged.png)
-
-This model was trained on 100,000 samples of random, uniform input data (in the range `[-10,10]`). It contains two hidden layers of 64 units each, using a softmax activation function. The final output layer was linear. The loss function used was the Mean Squared Error. In summary:
-
-| Hidden Layers | Hidden Layers Depth | Activation | Samples     | Loss | Epochs |
-|--------------|--------------|-----------|------------|------------|------------|
-| 2 | [ 64, 64] | Softmax | 100,000      | MSE     | 5 |
-
-Changing these hyperparameters can have a large impact on the ability of the NN to correctly approximate a function. There are some basic functions however which such a basic NN may fail to correctly approximate, despite experimentation with hyperparameters: where the outputs may be a distribution (i.e. a range of possible outputs).
-
-# 2. Modeling Outputs as Distributions
+# Modeling Outputs as Distributions
 
 To clarify the above statement, take the example of a function which includes a random variable:
 
@@ -37,4 +25,4 @@ In fact, a much smaller network can achieve the same output. Following the paper
 We can now consider the "inverse" problem, of predicting the input `x` based on the output `f(x)`. This is essentially flipping the inputs/outputs to the NN and observing if the model can approximated this inverse function. Increasing the depth of the model to 20 units in the hidden layer (the optimally tuned recommendation by Bishop, et. al), it is still not possible (note these results do not replicate Bishop, however it is clear from the paper it is not possible): 
 
 
-![Approximating `x` - backward pass](/images/06_1x20tanh_1000samples_1000epochs.png)
+![Approximating `x` - backward pass](/images/07_1x20_CNN.png)
--- main.py
+++ main.py
@@ -3,9 +3,13 @@
 
 import tensorflow as tf
 import models as ml
+import numpy as np
 import data_generator as dg
 from data_generator import generate_data_sinx
 from utilities import save_history, load_history, plot_histories, plot_xy, plot_metric
+
+import tensorflow_probability as tfp
+tfd = tfp.distributions
 
 ####################################################################################
 ####################################################################################
@@ -77,10 +81,10 @@ def train_and_predict_backward_pass(epochs):
     model = toy_problem_fx(dataset, epochs, 20)
     
     # prediction
-    x = tf.linspace(0, 1, 100)
-    y1 = dg.sinusoid_random(x)
-    y = model.predict(tf.transpose([x]))
-    plot_metric([y, y1], [x, x], "Plot of neural net approximating f(x)", ["nn(x)", "f(x)"])
+    t = tf.linspace(0, 1, 100)
+    x_actual = dg.sinusoid_random(t)
+    x_model = model.predict(tf.transpose([x_actual]))
+    plot_metric([x_model, x_actual], [t, t], "Plot of neural net approximating f(x)", ["nn(x)", "f(x)"])
 
 def train_and_predict_MDN(epochs):
     # dataset
@@ -99,10 +103,116 @@ def train_and_predict_MDN(epochs):
     print("Achieved loss: {}, accuracy: {} ".format(l, acc))
 
     # prediction
-    x = tf.linspace(0, 1, 100)
-    y1 = dg.sinusoid_random(x)
-    y = model.predict(tf.transpose([x]))
-    plot_metric([y, y1], [x, x], "Plot of neural net approximating f(x)", ["nn(x)", "f(x)"])
+    t = tf.linspace(0, 1, 100)
+    x_actual = dg.sinusoid_random(t)
+    x_model = model.predict(tf.transpose([x_actual]))
+    # o = y[0]
+    # mu_s = o[0:3]
+    # alpha_s = o[3:6]
+    # sigma_s = o[6:]
+    d = mdn_max_prediction(x_model)
+    alphas = tf.nn.softmax(x_model[:,3:6])
+    plot_metric([t, t, t ], [ alphas[:, 0], alphas[:, 1], alphas[:, 2]], "Plot of priors a_i(x)", ["a_1(x)", "a_2(x)", "a_3(x)"])
+    plot_gaussian(t, x_model[0, 0], x_model[0, 6])
+    
+    # d = mdn_run_prediction(x, y)
+    plot_metric([d, x_actual], [t, t], "Plot of neural net approximating f(x)", ["nn(x)", "f(x)"])
+
+def mdn_max_prediction(y):
+    # Eq. 49 in MDN, find the index (i) of the maximum alpha
+    # Choose the i_th mean as a prediction of network
+    alphas = y[:, 3:6]
+    args = tf.math.argmax(alphas, axis=1)
+    retval = []
+    for i in range(y.shape[0]):
+        retval.append(y[i, args[i]])
+    return tf.Variable(retval)
+
+def mdn_run_prediction(x, y):
+    retval = []
+    for i in range(len(x)):
+        mu_s = y[i, 0:3]
+        sigma_s = y[i, 6:]
+        v = mdn_prediction_single(x[i], mu_s, sigma_s)
+        retval.append(v)
+    return tf.Variable(retval)
+
+def mdn_prediction_single(x_i, mu_i, sigma_i):
+    x_i = tf.cast(x_i, tf.float32)
+    sigma_i = sigma_i**2
+    sigma_i = tf.linalg.diag(sigma_i)
+    mvn = tfd.MultivariateNormalTriL(loc=mu_i, scale_tril=tf.linalg.cholesky(sigma_i))
+    # mvn = tfd.MultivariateNormalFullCovariance(loc=mu, covariance_matrix=sigma)
+    return mvn.prob([x_i])
+
+def mdn_prediction(x_i, mu_i, sigma_i):
+    x_i = tf.cast(x_i, tf.float32)
+    x_i = tf.expand_dims(x_i, 1)
+    mu_i = tf.expand_dims(mu_i, 0)
+    sigma_i = sigma_i**2
+    sigma_i = tf.linalg.diag(sigma_i)
+    sigma_i = tf.expand_dims(sigma_i, 0)
+    mvn = tfd.MultivariateNormalTriL(loc=mu_i, scale_tril=tf.linalg.cholesky(sigma_i))
+    # mvn = tfd.MultivariateNormalFullCovariance(loc=mu, covariance_matrix=sigma)
+    return mvn.prob(x_i)
+
+def gaussian_1d(x, mu, sigma):
+    variance = sigma**2
+    return 1 / (2.0 * np.pi * variance) * np.exp(-( (x-mu)**2 / (2.0 * variance)) )
+
+def plot_gaussian(x, mu, sigma):
+    y = gaussian_1d(x, mu, sigma)
+    # y = np.random.normal(mu, sigma**2, len(x))
+    plot_metric([x], [y], "Plot of Gaussian (mu={},sigma={})".format(mu, sigma), ["Gaussian dist."])
+
+# def custom_training_and_predict_MDN():
+#     optimizer = tf.keras.optimizers.Adam(learning_rate=args.lr)
+
+#     train_loss = tf.keras.metrics.Mean(name='train_loss')
+#     # tb_callback.set_model(nn_model)
+
+
+#     loss = ml.MixtureDensityModelError(num_kernels=3)
+
+
+#     @tf.function
+#     def train_step(x, y):
+#         ######### Your code starts here #########
+#         # We want to perform a single training step (for one batch):
+#         # 1. Make a forward pass through the model
+#         # 2. Calculate the loss for the output of the forward pass
+#         # 3. Based on the loss calculate the gradient for all weights
+#         # 4. Run an optimization step on the weights.
+#         # Helpful Functions: tf.GradientTape(), tf.GradientTape.gradient(), tf.keras.Optimizer.apply_gradients
+#         with tf.GradientTape() as tape:
+#             # forward pass
+#             y_est = nn_model(x, training=True) # use dropout
+#             # compute the loss
+#             current_loss = loss(y_est, y)
+#         grads = tape.gradient(current_loss, nn_model.trainable_variables)
+#         optimizer.apply_gradients(zip(grads, nn_model.trainable_variables))
+#         ########## Your code ends here ##########
+
+#     train_loss(current_loss)
+
+#     @tf.function
+#     def train(train_data):
+#         for x, y in train_data:
+#             train_step(x, y)
+
+
+#     train_data = tf.data.Dataset.from_tensor_slices((data['x_train'], data['y_train'])).shuffle(100000).batch(params['train_batch_size'])
+
+#     losses = []
+#     for epoch in range(args.epochs):
+#         # Reset the metrics at the start of the next epoch        
+#         train_loss.reset_states()
+
+#         train(train_data)
+
+#         template = 'Epoch {}, Loss: {}'
+#         print(template.format(epoch + 1, train_loss.result()))
+#         losses.append(train_loss.result())
 
 ####################################################################################
 ####################################################################################
@@ -113,10 +223,10 @@ def train_and_predict_MDN(epochs):
 
 def main():
     tf.compat.v1.enable_eager_execution()
-    epochs = 3
+    epochs = 1000
     # train_and_predict_basic_nn(epochs)
     # train_and_predict_forward_pass(epochs)
-    # train_and_predict_backward_pass(epochs)
-    train_and_predict_MDN(epochs)
+    train_and_predict_backward_pass(epochs)
+    # train_and_predict_MDN(epochs)
 
 main()
